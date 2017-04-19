2d8c56bc133e48b2c4946545ddd2beb4725e8178
Arjo Bruijnes <arjo@weirdwater.net>
2017-04-19 10:33:52 +0200

    Ejected react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.app}>
+        <h1>hi</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
